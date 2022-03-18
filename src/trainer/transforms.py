eb8bd0649f5f0325e327c77a6bb64ef2bf891b23
Lakoc <xpolok03@stud.fit.vutbr.cz>
2022-03-18 11:51:36 +0100

    Augmentation pipeline, custom transforms

--- src/trainer/transforms.py
+++ src/trainer/transforms.py
@@ -15,30 +15,58 @@
     CT Scans of Human Abdomens' for KNN/2021L course.
 """
 
-import elasticdeform
 from torchvision import transforms as T
 import torch
+import numpy as np
+import numbers
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
 
 
-class ElasticTransform(torch.nn.Module):
-    """Elastic deformation of images as described in [Simard2003]_ (with modifications).
-       .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
-            Convolutional Neural Networks applied to Visual Document Analysis", in
-            Proc. of the International Conference on Document Analysis and
-            Recognition, 2003.
+class RandomElastic(object):
+    """Random Elastic transformation by CV2 method on image by alpha, sigma parameter.
+    Adopted from https://github.com/gatsby2016/Augmentation-PyTorch-Transforms.
+    Args:
+        alpha (float): alpha value for Elastic transformation, factor
+        if alpha is 0, output is original whatever the sigma;
+        if alpha is 1, output only depends on sigma parameter;
+        if alpha < 1 or > 1, it zoom in or out the sigma's Relevant dx, dy.
+        sigma (float): sigma value for Elastic transformation, should be \ in (0.05,0.1)
+        mask (PIL Image) in __call__, if not assign, set None.
+    """
 
-        Based on https://gist.github.com/erniejunior/601cdf56d2b424757de5
-       """
+    def __init__(self, alpha, sigma):
+        assert isinstance(alpha, numbers.Number) and isinstance(sigma, numbers.Number), \
+            "alpha and sigma should be a single number."
+        assert 0.05 <= sigma <= 0.1, \
+            "In pathological image, sigma should be in (0.05,0.1)"
+        self.alpha = alpha
+        self.sigma = sigma
 
-    def __init__(self, displacement):
-        super().__init__()
-        self.displacement = displacement
+    @staticmethod
+    def RandomElasticCV2(img, alpha, sigma):
+        alpha = img.shape[1] * alpha
+        sigma = img.shape[1] * sigma
 
-    def __call__(self, image):
-        return elasticdeform.deform_grid(image, self.displacement)
+        shape = img.shape
+
+        dx = gaussian_filter((np.random.rand(*shape) * 2 - 1), sigma) * alpha
+        dy = gaussian_filter((np.random.rand(*shape) * 2 - 1), sigma) * alpha
+
+        x, y, z = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]), np.arange(shape[2]))
+        indices = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1)), np.reshape(z, (-1, 1))
+
+        img = map_coordinates(img, indices, order=0, mode='reflect').reshape(shape)
+        return img
+
+    def __call__(self, img):
+        return self.RandomElasticCV2(img[..., np.newaxis], self.alpha, self.sigma)
 
     def __repr__(self):
-        return "Elastic transform"
+        format_string = self.__class__.__name__ + '(alpha value={0})'.format(self.alpha)
+        format_string += ', sigma={0}'.format(self.sigma)
+        format_string += ')'
+        return format_string
 
 
 class Invert(torch.nn.Module):
