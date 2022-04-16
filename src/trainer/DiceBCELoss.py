35d5312ff8b9dce5976454e121235344b80ad0b3
Tibor Kubik <kubik@t3d.team>
2022-04-16 15:48:02 +0200

    Dice Loss combined with the Binary Cross Entropy.

---
+++ src/trainer/DiceBCELoss.py
@@ -0,0 +1,40 @@
+"""
+    :filename DiceBCELoss.py
+
+    :brief Dice Loss combined with the Binary Cross-Entropy for medical image segmentation.
+
+    :author Tibor Kubik
+    :author Ladislav Ondris
+    :author Alexander Polok
+
+    :email xkubik34@stud.fit.vutbr.cz
+    :email xondri07@sstud.fit.vutbr.cz
+    :email xpolok03@stud.fit.vutbr.cz
+
+    This file was created as a part of project called 'Visual Transformers for Liver and Liver Tumor Segmentation from
+    CT Scans of Human Abdomens' for KNN/2021L course.
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DiceBCELoss(nn.Module):
+    def __init__(self):
+
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        # comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)
+
+        # flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()
+        dice_loss = 1 - (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+
+        return Dice_BCE
